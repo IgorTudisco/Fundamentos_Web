8b808f8da5e74ed1976681c6fc5d188eda07f0e9
Igor Tudisco <igortudisco@hotmail.com>
2021-07-21 11:00:59 -0300

    terminando o js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ let habilidade = document.querySelector('#habilidade')
 let assunto = document.querySelector('#assunto')
 let txtNome = document.querySelector('#txtNome')
 let txtHabilidade = document.querySelector('#txtHabilidade')
+let txtAssunto = document.querySelector('#txtAssunto')
+let mapa = document.querySelector('#mapa')
+
+// váriavel para validar o envio
+
+let nomeOk = false
+let habilidadeOk = false
+let assuntoOk = false
 
 function validaNome() {
   let verificaNum = !isNaN(nome.value)
@@ -18,20 +26,55 @@ function validaNome() {
   if (nome.value.length < 3 || verificaNum || stringNum) {
     txtNome.innerHTML = 'Nome Inválido'
     txtNome.style.color = 'red'
+    nomeOk = false
   } else {
     txtNome.innerHTML = 'Nome Válido'
     txtNome.style.color = 'green'
+    nomeOk = true
   }
 }
 
 function validaHabilidade() {
   /* Caso exista esse elemento dentro da abilidade ele irá retornar -1 
     é mais usado para validar o email.*/
-  if (habilidade.value.indexOf('@') == -1) {
+  if (
+    habilidade.value.indexOf('@') == -1 ||
+    habilidade.value.indexOf('.') == -1
+  ) {
     txtHabilidade.innerHTML = 'Invalido'
     txtHabilidade.style.color = 'red'
+    habilidadeOk = false
   } else {
     txtHabilidade.innerHTML = 'Valido'
     txtHabilidade.style.color = 'green'
+    habilidadeOk = true
   }
+}
+
+function validaAssunto() {
+  if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres'
+    txtAssunto.style.color = 'red'
+    txtAssunto.style.display = 'block'
+    assuntoOk = false
+  } else {
+    txtAssunto.style.display = 'none'
+    assuntoOk = true
+  }
+}
+
+function validaEnvio() {
+  if (nomeOk && habilidadeOk && assuntoOk) {
+    alert('Formulário enviado')
+  } else {
+    alert('Preencha o formulário corretamente antes de enviar...')
+  }
+}
+
+function zoomMapa() {
+  mapa.style.width = '70%'
+}
+
+function zoomNomalMapa() {
+  mapa.style.width = '35%'
 }
